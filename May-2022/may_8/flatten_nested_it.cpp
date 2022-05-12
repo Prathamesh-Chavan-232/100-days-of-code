ddf91d8f2f9d6ec9c9b20f3f59c5437b2ab587dd
Prathamesh Chavan <prathamesh.chavanpsc2018@gmail.com>
2022-05-13 05:28:47 +0530

    8th may documentation

--- May-2022/may_8/flatten_nested_it.cpp
+++ May-2022/may_8/flatten_nested_it.cpp
@@ -7,43 +7,130 @@ using namespace std;
 #define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
 #define pb push_back
 #define mp make_pair
+#define pqb priority_queue<int>
+#define pqs priority_queue<int, vector<int>, greater<int>>
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define foreach(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define formap(m) for (auto [key, value] : m)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 // Typdefs for containers
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
 typedef vector<int> vi;
 typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef map<int, int> mii;
+typedef map<ll, ll> ml;
+typedef set<ll> sl;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<ll, ll> uml;
+typedef unordered_set<ll> usl;
 
+// Debugging tools
+#ifndef ONLINE_JUDGE
+#define debug(...) logger(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...)
+#endif
 
+#ifndef ONLINE_JUDGE
+#define debcon(x)        \
+    cerr << #x << " = "; \
+    _print(x);           \
+    cerr << "\n";
+#else
+#define debcon(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debmap(x)        \
+    cerr << #x << " = "; \
+    _printmap(x);        \
+    cerr << "\n";
+#else
+#define debmap(x)
+#endif
+
+// Function definitions
+template <typename... Args>
+void logger(string varname, Args &&...values) // logger for varadiac debugging print statements
+{
+
+    cerr << varname << " = ";
+    string delim = "";
+    (..., (cerr << delim << values, delim = ", "));
+    cerr << "\n";
+}
+
+template <typename T>
+void _print(T const &c) // print any type of vector / set
+{
+    cerr << "{ ";
+    foreach (it, c)
+        cerr << *it << ", ";
+    cerr << "}";
+}
+
+template <typename T, typename U>
+void _printmap(map<T, U> m)
+{
+    cerr << "{ ";
+    formap(m)
+    {
+        cerr << "{" << key << "," << value << "} ";
+    }
+    cerr << "}";
+}
+// Varadiac I/O
+template <typename... T>
+void r(T &...args)
+{
+    ((cin >> args), ...);
+}
+template <typename... T>
+void w(T &&...args)
+{
+    ((cout << args << " "), ...);
+    cout << "\n";
+}
 // constants
 const int mod = 1'000'000'007;
 const int N = 1e7, M = N;
 const double PI = 3.1415926535897932384626;
 
 /*
-    Link -
-    Problem -
-    Difficulty -
-    contest -
-    Status -
-    Date -
+    Link - https://leetcode.com/problems/flatten-nested-list-iterator/
+    Problem - Flatten nested iterator (dfs & stack)
+    Difficulty - Easy (dfs), Medium (stack) 
+    contest - daily leetcode may 2022
+    Status - solved + hints (dfs),
+    Date - 09/5/22
 */
 /*  Approach -
+    Ignore the NestedInteger not defined errors (its an ADT) code was accepted on leetcode.
 
 */
 
 /**
- * // This is the interface that allows for creating nested lists.
- * // You should not implement it, or speculate about its implementation
+ *  This is the interface that allows for creating nested lists.
+ *  You should not implement it, or speculate about its implementation
  * class NestedInteger {
  *   public:
- *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
  *     bool isInteger() const;
  *
- *     // Return the single integer that this NestedInteger holds, if it holds a single integer
- *     // The result is undefined if this NestedInteger holds a nested list
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     The result is undefined if this NestedInteger holds a nested list
  *     int getInteger() const;
  *
- *     // Return the nested list that this NestedInteger holds, if it holds a nested list
- *     // The result is undefined if this NestedInteger holds a single integer
+ *     Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     The result is undefined if this NestedInteger holds a single integer
  *     const vector<NestedInteger> &getList() const;
  * };
  */
@@ -52,17 +139,17 @@ class NestedIterator
 {
     int ct = 0;
     vector<int> flattenList;
-public:
 
+public:
     void dfs(NestedInteger x) // x -> integer || list
     {
-        if(x.isInteger())
+        if (x.isInteger())
         {
-            flattenList.pb(x.getInteger());       
+            flattenList.pb(x.getInteger());
         }
         else
         {
-            for(auto val : x.getList()) // again dfs on each value in the list
+            for (auto val : x.getList()) // again dfs on each value in the list
             {
                 dfs(val);
             }
@@ -84,7 +171,7 @@ public:
 
     bool hasNext()
     {
-        if(ct < flattenList.size())
+        if (ct < flattenList.size())
         {
             return true;
         }
@@ -97,3 +184,25 @@ public:
  * NestedIterator i(nestedList);
  * while (i.hasNext()) cout << i.next();
  */
+
+int main()
+{
+    auto start = chrono::steady_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("C:/Prathamesh/Programming/input.txt", "r", stdin);
+    freopen("C:/Prathamesh/Programming/output.txt", "w", stdout);
+    freopen("C:/Prathamesh/Programming/err.txt", "w", stderr);
+#endif
+    vector<int> nestedList;
+    NestedIterator s(nestedList);
+    int res; // store return value
+
+#ifndef ONLINE_JUDGE
+    auto end = chrono::steady_clock::now();
+    auto diff = end - start;
+    cerr << "[Finished in " << setprecision(3) << chrono::duration<double, milli>(diff).count() << " ms]\n";
+#endif
+    return 0;
+}
